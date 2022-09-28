a2aff39078dd834fc968d121c141852cf0bc89d8
Leoncio Pavón <leonciox5000@gmail.com>
2022-09-28 02:23:33 -0600

    Implementé los métodos click.

--- app/src/main/java/ni/edu/uca/listadoprod/MainActivity.kt
+++ app/src/main/java/ni/edu/uca/listadoprod/MainActivity.kt
@@ -2,6 +2,7 @@ package ni.edu.uca.listadoprod
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import ni.edu.uca.listadoprod.dataadapter.ProductoAdapter
@@ -11,13 +12,28 @@ import ni.edu.uca.listadoprod.dataclass.Producto
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    /*Arreglo dinámico de datos*/
     var listaProd = ArrayList<Producto>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        /*Llamamos la funcion limpiar*/
         iniciar()
     }
+
+    /*Funcion de inicio enla que le mandamos las funciones de agregar los productos a nuestro botón
+   * y a nuestro botón limpiar la funcion para eliminar datos de los edit text*/
+    private fun iniciar(){
+        binding.btnAddProd.setOnClickListener{
+            agregarProd()
+        }
+        binding.btnClear.setOnClickListener{
+            limpiar()
+        }
+    }
+     /*La siguiente funcion elimina los datos de los edit text*/
     private fun limpiar(){
         with(binding){
             etID.setText("")
@@ -26,7 +42,7 @@ class MainActivity : AppCompatActivity() {
             etID.requestFocus()
         }
     }
-
+    /*Con esta función podremos agregar productos a nuestra lista dinámica*/
     private fun agregarProd(){
         with(binding){
             try{
@@ -35,22 +51,52 @@ class MainActivity : AppCompatActivity() {
                 val precio: Double = etPrecio.text.toString().toDouble()
                 val prod = Producto(id, nombre, precio)
                 listaProd.add(prod)
+
+                /*Captura de errores*/
             } catch (ex: Exception){
                 Toast.makeText(this@MainActivity, "Error : ${ex.toString()}",
                     Toast.LENGTH_LONG).show()
             }
             rcvLista.layoutManager = LinearLayoutManager(this@MainActivity)
-            rcvLista.adapter = ProductoAdapter(listaProd)
+            rcvLista.adapter = ProductoAdapter(listaProd,
+                {producto -> onItemSelected(producto) },
+                {position -> onDeleteItem(position)},
+                {position -> onUpdateItem(position)})
             limpiar()
         }
     }
 
-    private fun iniciar(){
-        binding.btnAgregar.setOnClickListener{
-            agregarProd()
+    /*Funcion para poder editar items de la lista; es decir en este caso productos*/
+    fun onItemSelected(producto: Producto) {
+        with(binding) {
+            etID.text = producto.id.toString().toEditable()
+            etNombreProd.text = producto.nombre.toEditable()
+            etPrecio.text = producto.precio.toString().toEditable()
         }
-        binding.btnLimpiar.setOnClickListener{
+    }
+
+    fun String.toEditable(): Editable = Editable.Factory.getInstance().newEditable(this)
+
+
+    /*Funcion para eliminar items de la lista; es decir productos en este caso*/
+
+    fun onDeleteItem(position: Int){
+        with(binding){
+            listaProd.removeAt(position)
+            rcvLista.adapter?.notifyItemRemoved(position)
             limpiar()
+        }
+    }
+
+    /*La siguiente función es para poder editrar */
+    fun onUpdateItem(position: Int){
+        with(binding){
+            val id: Int = etID.text.toString().toInt()
+            val nombre: String = etNombreProd.text.toString()
+            val precio: Double = etPrecio.text.toString().toDouble()
+            val prod = Producto(id, nombre, precio)
+            listaProd.set(position, prod)
+            rcvLista.adapter?.notifyItemChanged(position)
         }
     }
 }
--- app/src/main/java/ni/edu/uca/listadoprod/dataadapter/ProductoAdapter.kt
+++ app/src/main/java/ni/edu/uca/listadoprod/dataadapter/ProductoAdapter.kt
@@ -1,35 +1,53 @@
 package ni.edu.uca.listadoprod.dataadapter
 
+import android.content.DialogInterface
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.NonDisposableHandle.parent
 import ni.edu.uca.listadoprod.databinding.ItemlistaBinding
 import ni.edu.uca.listadoprod.dataclass.Producto
 
-class ProductoAdapter(val listProd: List<Producto>) :
+/*Crearemos variable para la lista de prudctos, evento OnClick para que los campos reaparezccan en los Et
+  variable click para eliminar prod de la lista y  val on click para editar la lista de uno en uno*/
+class ProductoAdapter(val listProd: List<Producto>, private val onClickListener:(Producto) ->Unit,
+       private val onClickDelete: (Int) -> Unit,
+       private val onClickEdit: (Int) -> Unit):
+
+        /*Enlace entre los datos y un el adaptador vista */
     RecyclerView.Adapter<ProductoAdapter.ProductoHolder>() {
-        inner class ProductoHolder(val binding: ItemlistaBinding) :
-            RecyclerView.ViewHolder(binding.root) {
-            fun cargar(producto: Producto) {
-                with(binding) {
+        inner class ProductoHolder(val binding: ItemlistaBinding):
+            RecyclerView.ViewHolder(binding.root){
+              /*Cargamos los datos*/
+            fun cargar(producto: Producto,
+                       onClickListener: (Producto) -> Unit,
+                       onClickDelete: (Int) -> Unit,
+                       onClickEdit: (Int) -> Unit){
+                with(binding){
                     tvCodProd.text = producto.id.toString()
                     tvNombreProd.text = producto.nombre
                     tvPrecioProd.text = producto.precio.toString()
+                    /*Eventos Click*/
+                    itemView.setOnClickListener { onClickListener(producto) }
+                    binding.btnDelete.setOnClickListener { onClickDelete(adapterPosition) }
+                    binding.btnEdit.setOnClickListener{onClickEdit(adapterPosition)}
                 }
             }
         }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductoHolder {
+            /*Diseño individual de la vista de los productos*/
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductoHolder{
        val binding = ItemlistaBinding.inflate(
            LayoutInflater.from(parent.context), parent,
              false
        )
         return ProductoHolder(binding)
     }
-
-    override fun onBindViewHolder(holder: ProductoHolder, position: Int) {
-        holder.cargar(listProd[position])
+            /*Pasamos la posición del producto*/
+    override fun onBindViewHolder(holder: ProductoHolder, position: Int)  {
+        holder.cargar(listProd[position], onClickListener, onClickDelete, onClickEdit)
     }
-
+       /*Tamaño de la lista*/
     override fun getItemCount(): Int = listProd.size
 }
