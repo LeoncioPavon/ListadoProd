8d61ea905ba11318d7fe2d55dabf31d6e8c5b679
Leoncio Pavón <leoncio.pavon14107@est.uca.edu.ni>
2022-09-28 04:18:53 -0600

    ultimaversion

--- app/src/main/java/ni/edu/uca/listadoprod/dataclass/Producto.kt
+++ app/src/main/java/ni/edu/uca/listadoprod/dataclass/Producto.kt
@@ -1,7 +1,3 @@
 package ni.edu.uca.listadoprod.dataclass
 
-data class  Producto(
-    val id: Int,
-    val nombre: String,
-    val precio: Double
-)
+data class Producto (val id: Int, val nombre: String, val precio: Double)
